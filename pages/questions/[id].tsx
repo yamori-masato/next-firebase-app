8942dc4960c944ff97ec71ba04409a783994e7b8
yamori-masato <yamorimasato@gmail.com>
2021-05-14 03:29:46 +0900

    update questions/[id] page

--- pages/questions/[id].tsx
+++ pages/questions/[id].tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, FormEvent } from 'react'
 import { useRouter } from 'next/router'
 import firebase from 'firebase/app'
 import Layout from '../../components/Layout'
@@ -14,6 +14,8 @@ const QuestionsShow = () => {
   const query = router.query as Query
   const { user } = useAuthentication()
   const [question, setQuestion] = useState<Question>(null)
+  const [isSending, setIsSending] = useState<boolean>(false)
+  const [body, setBody] = useState('')
 
   useEffect(() => {
     (async function () {
@@ -36,6 +38,25 @@ const QuestionsShow = () => {
     })()
   }, [query.id])
 
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setIsSending(true)
+  
+    await firebase.firestore().runTransaction(async (t) => {
+      t.set(firebase.firestore().collection('answers').doc(), {
+        uid: user.uid,
+        questionId: question.id,
+        body,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      t.update(firebase.firestore().collection('questions').doc(question.id), {
+        isReplied: true,
+      })
+    })
+  
+    setIsSending(false)
+  }
+
   return (
     <Layout>
       <div className="row justify-content-center">
@@ -45,6 +66,29 @@ const QuestionsShow = () => {
               <div className="card-body">{question.body}</div>
             </div>
           )}
+          <section className="text-center mt-4">
+            <h2 className="h4">回答する</h2>
+
+            <form onSubmit={onSubmit}>
+              <textarea
+                className="form-control"
+                placeholder="おげんきですよ"
+                rows={6}
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                required
+              ></textarea>
+              <div className="m-3">
+                {isSending ? (
+                  <div className="spinner-border text-secondary" role="status"></div>
+                ) : (
+                  <button type="submit" className="btn btn-primary">
+                    回答する
+                  </button>
+                )}
+              </div>
+            </form>
+          </section>
         </div>
       </div>
     </Layout>
