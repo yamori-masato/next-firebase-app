40dcddb3b77d626db9ea6bdad0cf8393ae5f5390
yamori-masato <yamorimasato@gmail.com>
2021-05-13 22:59:16 +0900

    update Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,20 @@
 import { FC } from 'react'
+import dayjs from 'dayjs'
 
 type Props = {
   text: string,
+  date: string,
 }
 
-const Card: FC<Props> = ({text}) => {
+const Card: FC<Props> = ({text, date}) => {
   return (
     <div>
       <div className="card my-3">
         <div className="card-body">
           <div className="text-truncate">{text}</div>
+          <div className="text-muted text-end">
+            <small>{date}</small>
+          </div>
         </div>
       </div>
     </div>
